e8ef2a8d24450fdc88d94024e20f657380bd2f19
vid <vid@ebi.ac.uk>
2021-02-13 19:23:34 +0000

    Initial client with vuex, i18n and google OAuth

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <div id="wrapper">
+        <Header/>
+        <NavBar/>
+        <main>
+          <LeftNav/>
+          <router-view/> 
+        </main>
+        <Footer/>
+      </div>
     </div>
-    <router-view />
-  </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+import NavBar from '@/components/NavBar.vue';
+import LeftNav from '@/components/LeftNav.vue';
+import Footer from '@/components/Footer.vue';
+/*import {useI18n} from "vue-i18n";*/
+export default {
+  name: 'app',
+/*  data() {
+      const {t, locale} = useI18n();
+      return {t, locale};
+  },*/
+  components: {
+    Header,
+    NavBar,
+    LeftNav,
+    Footer
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import "css/main.css";
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,30 +1,49 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/BookSearch.vue'
+import Login from '@/components/Login.vue'
+import JoinUs from '@/components/joinUs.vue'
+import About from '@/components/AboutUs.vue'
+import BookDetail from '@/components/BookDetail.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/joinUs',
+    name: 'JoinUs',
+    component: JoinUs
+  },
+  {
+    path:'/bookDetail',
+    name: 'BookDetail',
+    component: BookDetail
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    //import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-];
+]
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
